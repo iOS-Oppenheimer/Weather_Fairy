f0dd35c8b7ecf3270f93739a46b1b3afdd762e1c
Luna828 <eskw0701@naver.com>
2023-09-25 17:17:29 +0900

    [FEAT]: Navigation Bar Item 추가 및 Asset에 icon 추가

--- Weather_Fairy/Weather_Fairy/View/MainViewController.swift
+++ Weather_Fairy/Weather_Fairy/View/MainViewController.swift
@@ -1,12 +1,41 @@
+import SwiftUI
 import UIKit
 
 class MainViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+
+        
+        let presentLocationBarItem = UIBarButtonItem.presentLocationItemButton(target: self, action: #selector(presentLocationTapped))
+        navigationItem.leftBarButtonItem = presentLocationBarItem
+
+        let menuBarItem = UIBarButtonItem.menuItemButton(target: self, action: #selector(menuTapped))
+        navigationItem.rightBarButtonItem = menuBarItem
+    } //: viewDidLoad()
+    
+// ========================================🔽 navigation Bar Tapped구현==========================================
+    @objc func presentLocationTapped(){}
+    
+    @objc func menuTapped(){}
+} //: UIViewController
+
+struct MainViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        MainVCRepresentable()
+            .edgesIgnoringSafeArea(.all)
     }
-
-
 }
 
+struct MainVCRepresentable: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> UIViewController {
+        let mainViewController = MainViewController()
+        return UINavigationController(rootViewController: mainViewController)
+    }
+
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        // You can add update logic here if needed
+    }
+
+    typealias UIViewControllerType = UIViewController
+}
