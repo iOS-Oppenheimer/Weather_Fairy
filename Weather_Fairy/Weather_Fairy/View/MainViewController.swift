e177ea6295e283e114fafeca37ec56a2d6405739
SangWoo Park <136118540+angwoo0503@users.noreply.github.com>
2023-09-26 10:22:31 +0900

    [Feat]: SearchPage UI 구성

--- Weather_Fairy/Weather_Fairy/View/MainViewController.swift
+++ Weather_Fairy/Weather_Fairy/View/MainViewController.swift
@@ -1,12 +1,49 @@
 import UIKit
+import SwiftUI
 
 class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        
+
+        let goToSearchButton = UIButton(type: .system)
+        goToSearchButton.setTitle("검색 페이지로 이동", for: .normal)
+        goToSearchButton.addTarget(self, action: #selector(goToSearchPage), for: .touchUpInside)
+        view.addSubview(goToSearchButton)
+        
+
+        goToSearchButton.snp.makeConstraints { make in
+            make.centerX.centerY.equalToSuperview()
+        }
+        
     }
 
+    @objc func goToSearchPage() {
+        let searchViewController = SearchPageViewController()
+        navigationController?.pushViewController(searchViewController, animated: true)
+    }
+   
 
 }
 
+struct MainViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        MainVCRepresentable()
+            .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct MainVCRepresentable: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> UIViewController {
+        let mainViewController = MainViewController()
+        return UINavigationController(rootViewController: mainViewController)
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        // You can add update logic here if needed
+    }
+    
+    typealias UIViewControllerType = UIViewController
+}
