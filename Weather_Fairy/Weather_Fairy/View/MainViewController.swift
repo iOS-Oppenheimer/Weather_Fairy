ff757623f1cab42ce1bdab02e68a1072aa7b2720
Luna828 <eskw0701@naver.com>
2023-09-26 10:27:18 +0900

    [FEAT]: MainVC에 View를 합칠 file 생성

--- Weather_Fairy/Weather_Fairy/View/MainViewController.swift
+++ Weather_Fairy/Weather_Fairy/View/MainViewController.swift
@@ -11,7 +11,6 @@ class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-
         locationView.locationManager.delegate = self
 
         // NavigationBarButton 구현
