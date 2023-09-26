99c6570473b7870a3a0a1858fab4b36cd421c08d
churoo <churoo0418@gmail.com>
2023-09-26 10:50:31 +0900

    [FEAT]앱 실행시 notification 설정 허용 거부 표시

--- Weather_Fairy/Weather_Fairy/Resources/AppDelegate.swift
+++ Weather_Fairy/Weather_Fairy/Resources/AppDelegate.swift
@@ -1,10 +1,20 @@
 import UIKit
+import UserNotifications
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+
+        UNUserNotificationCenter.current().delegate = self
+
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { granted, _ in
+            if granted {
+                print("로컬 푸시 알림 권한이 허용")
+            } else {
+                print("로컬 푸시 알림 권한이 거부")
+            }
+        }
         return true
     }
 
@@ -21,7 +31,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
 
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    // 알림이 표시될 때
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        // 앱이 활성 상태일 때 푸시 알림 표시
+        completionHandler([.alert, .sound, .badge])
+    }
+
+    // 푸시 알림을 클릭했을 때
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+    }
+}
