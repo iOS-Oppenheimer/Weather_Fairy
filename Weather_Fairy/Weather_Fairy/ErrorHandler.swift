39912dc4b5e4ddb1d00c11dc923f2b63f6d9ed35
SangWoo Park <136118540+angwoo0503@users.noreply.github.com>
2023-09-27 21:33:40 +0900

    [FEAT]: Font 파일, ErrorHandler 추가

---
+++ Weather_Fairy/Weather_Fairy/ErrorHandler.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+enum Result<Success, Failure: Error> {
+     case success(Success)
+     case failure(Failure)
+ }
+
+ enum APIError: Error {
+     case invalidResponse
+     case noCityName
+     case noData
+     case invalidJSON
+     case failedRequest
+     case invalidData
+     case failedResponse
+ }
