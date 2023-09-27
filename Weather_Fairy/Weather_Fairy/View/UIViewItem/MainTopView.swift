f7930810e18f944799aeaa11ffdc4551b8e635b6
원성준 <tjdwns575859@gmail.com>
2023-09-27 20:26:14 +0900

    [FEAT]: MainPage UI 레이아웃 지정

--- Weather_Fairy/Weather_Fairy/View/UIViewItem/MainTopView.swift
+++ Weather_Fairy/Weather_Fairy/View/UIViewItem/MainTopView.swift
@@ -11,16 +11,7 @@ class TopView: UIView {
 
     lazy var celsiusLabel: UILabel = {
         let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "21"
-        label.font = UIFont.systemFont(ofSize: 100, weight: .bold)
-        label.sizeToFit()
-        label.textAlignment = .center
-        label.textColor = .systemBackground
-        label.layer.shadowColor = UIColor.black.cgColor
-        label.layer.shadowOffset = CGSize(width: 0, height: 2)
-        label.layer.shadowOpacity = 0.8
-        label.layer.shadowRadius = 2
+        label.topLabel(text: "21", font: UIFont.systemFont(ofSize: 100, weight: .bold), textColor: .systemBackground)
 
         return label
     }()
@@ -92,7 +83,7 @@ class TopView: UIView {
         button.layer.borderColor = UIColor.systemBackground.cgColor
         button.layer.borderWidth = 0
         button.layer.cornerRadius = 5
-        // button.addTarget(self, action: #selector(signChangeButtonTapped), for: .touchUpInside)
+        button.addTarget(self, action: #selector(signChangeButtonTapped), for: .touchUpInside)
 
         return button
     }()
@@ -105,7 +96,7 @@ class TopView: UIView {
         label.sizeToFit()
         label.textAlignment = .center
         label.textColor = .systemBackground
-        label.layer.shadowColor = UIColor.black.cgColor
+        label.layer.shadowColor = UIColor.systemBackground.cgColor
         label.layer.shadowOffset = CGSize(width: 0, height: 2)
         label.layer.shadowOpacity = 0.8
         label.layer.shadowRadius = 2
@@ -117,7 +108,7 @@ class TopView: UIView {
         let stackView = UIStackView(arrangedSubviews: [celsiusLabel, celsiusSignLabel])
         stackView.horizontalStackView(spacing: 2)
         stackView.alignment = .center
-        
+
         return stackView
     }()
 
@@ -127,6 +118,7 @@ class TopView: UIView {
         stackView.alignment = .center
         stackView.axis = .horizontal
         stackView.spacing = 2
+        stackView.isHidden = true
 
         return stackView
     }()
@@ -154,44 +146,25 @@ class TopView: UIView {
 
     private func setupConstraints() {
         addSubview(topStackView)
-        topStackView.translatesAutoresizingMaskIntoConstraints = false
-//        addSubview(conditionsLabel)
-//        addSubview(signChangeButton)
-//        addSubview(cityName)
-//        addSubview(temperatureStackView)
-//        conditionsLabel.translatesAutoresizingMaskIntoConstraints = false
-//        signChangeButton.translatesAutoresizingMaskIntoConstraints = false
-//        cityName.translatesAutoresizingMaskIntoConstraints = false
-//        temperatureStackView.translatesAutoresizingMaskIntoConstraints = false
 
-        let safeArea = safeAreaLayoutGuide
+        topStackView.translatesAutoresizingMaskIntoConstraints = false
 
         NSLayoutConstraint.activate([
-            topStackView.centerXAnchor.constraint(equalTo: safeArea.centerXAnchor)
-//            cityName.centerXAnchor.constraint(equalTo: safeArea.centerXAnchor),
-//            cityName.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 30),
-//            cityName.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 40),
-//            cityName.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -40),
-//            cityName.heightAnchor.constraint(equalToConstant: 35),
-//            cityName.widthAnchor.constraint(equalToConstant: 70),
-//
-//            temperatureStackView.centerXAnchor.constraint(equalTo: safeArea.centerXAnchor),
-//            temperatureStackView.topAnchor.constraint(equalTo: cityName.bottomAnchor, constant: 5),
-//            temperatureStackView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 30),
-//            temperatureStackView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -30),
-//            temperatureStackView.heightAnchor.constraint(equalToConstant: 120),
-//
-//            signChangeButton.topAnchor.constraint(equalTo: temperatureStackView.bottomAnchor, constant: -10),
-//            signChangeButton.rightAnchor.constraint(equalTo: temperatureStackView.rightAnchor, constant: -20),
-//
-//            conditionsLabel.centerXAnchor.constraint(equalTo: safeArea.centerXAnchor),
-//            conditionsLabel.topAnchor.constraint(equalTo: signChangeButton.bottomAnchor, constant: 10),
+            topStackView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            topStackView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor),
+            topStackView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor),
+            topStackView.bottomAnchor.constraint(lessThanOrEqualTo: safeAreaLayoutGuide.bottomAnchor) // 추가된 제약 조건
         ])
+    }
+
+    @objc func signChangeButtonTapped() {
+        celsiusStackView.isHidden = !celsiusStackView.isHidden
+        fahrenheitStackView.isHidden = !fahrenheitStackView.isHidden
     }
 
     override init(frame: CGRect) {
         super.init(frame: frame)
-        // setupConstraints()
+        setupConstraints()
     }
 
     @available(*, unavailable)
