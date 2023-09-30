e58ed1dfb6ae56f128e0b250b1aa02c71bf42861
Luna828 <eskw0701@naver.com>
2023-09-30 18:01:03 +0900

    [FEAT]: 로직 변경 및 viewModel + MainView 연결 (코드량 감소)

--- Weather_Fairy/Weather_Fairy/View/MainViewController.swift
+++ Weather_Fairy/Weather_Fairy/View/MainViewController.swift
@@ -6,34 +6,32 @@ import UIKit
 
 class MainViewController: UIViewController, MiddleViewDelegate {
     private var viewModel: MainViewModel?
-    let locationManager = CLLocationManager()
+    private let locationManager = CLLocationManager()
+    private let notificationForUmbrella = NotificationForUmbrella() // 박철우
     
-    let notificationForUmbrella = NotificationForUmbrella() // 박철우
-    let bottomMyLocationView = BottomMyLocationView()
-    let bottomWeatherForecastView = BottomWeatherForecastView()
-    let bottomCurrentWeatherView = BottomCurrentWeatherView()
-    let middleView = MiddleView()
-    let topView = TopView()
+    private let mainView = MainView()
+    private let currentWeather: BottomCurrentWeatherView
+    private let forecast: BottomWeatherForecastView
+    private let myLocation: BottomMyLocationView
+    override func loadView() {
+        view = mainView
+    }
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // MainViewModel 인스턴스 생성 및 초기화
-        viewModel = MainViewModel(locationManager: locationManager, mapView: bottomMyLocationView.mapkit.customMapView)
-        // viewModel?.delegate = self
-
-        middleView.delegate = self
-        // mapview delegate 설정 : mapMaker 디자인을 위해서!
-        bottomMyLocationView.mapkit.customMapView.delegate = self
         view.backgroundColor = .systemBackground
-        bottomMyLocationView.mapkit.locationManager.delegate = self
+        MainNavigationBar.setupNavigationBar(for: self, resetButton: #selector(resetLocationButtonTapped), searchPageButton: #selector(SearchPageButtonTapped))
+        // MainViewModel 인스턴스 생성 및 초기화
+        viewModel = MainViewModel(locationManager: locationManager, mapView: mainView.bottomMyLocationView.mapkit.customMapView)
+        // viewModel?.delegate = self
+        mainView.middleView.delegate = self
+        // mapview delegate 설정 : mapMaker 디자인을 위해서!
+        myLocation.mapkit.customMapView.delegate = self
+        myLocation.mapkit.locationManager.delegate = self
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation()
-        setupNavigationBar()
-        setupBackgroundImage()
-        setupViews()
     }
 
     override func viewDidAppear(_ animated: Bool) { // 박철우
@@ -42,95 +40,6 @@ class MainViewController: UIViewController, MiddleViewDelegate {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-    }
-
-    private func setupViews() {
-        view.addSubview(topView)
-        view.addSubview(middleView)
-        view.addSubview(bottomCurrentWeatherView)
-        view.addSubview(bottomMyLocationView)
-        view.addSubview(bottomWeatherForecastView)
-
-        // view.addSubview(locationView)
-
-        // topView 위치, 크기 설정
-        topView.frame = CGRect(x: 0, y: 110, width: UIScreen.main.bounds
-            .width, height: 230)
-
-        // middleView 위치, 크기 설정
-        let middleHeight: CGFloat = 50
-        let middleYPosition = topView.frame.origin.y + topView.frame.height
-        middleView.frame = CGRect(x: 0, y: middleYPosition, width: UIScreen.main.bounds.width, height: middleHeight)
-
-        // 현재 날씨 뷰의 위치, 크기 설정
-        let bottomCurrentWeatherViewHeight: CGFloat = 350
-        let bottomCurrentWeatherViewHeightYPosition = middleYPosition + middleHeight
-        bottomCurrentWeatherView.frame = CGRect(x: 0, y: bottomCurrentWeatherViewHeightYPosition, width: UIScreen.main.bounds.width, height: bottomCurrentWeatherViewHeight)
-
-        // 기상 예보 뷰의 위치, 크기 설정
-        let bottomWeatherForecastViewHeight: CGFloat = 350
-        let bottomWeatherForecastViewYPosition = middleYPosition + middleHeight
-        bottomWeatherForecastView.frame = CGRect(x: 0, y: bottomWeatherForecastViewYPosition, width: UIScreen.main.bounds.width, height: bottomWeatherForecastViewHeight)
-
-        // 나의 위치 뷰의 위치, 크기 설정
-        let bottomMyLocationViewHeight: CGFloat = 350
-        let bottomMyLocationViewYPosition = middleYPosition + middleHeight
-        bottomMyLocationView.frame = CGRect(x: 0, y: bottomMyLocationViewYPosition, width: UIScreen.main.bounds.width, height: bottomMyLocationViewHeight)
-    }
-
-    private func setupBackgroundImage() {
-        if let backgroundImage = UIImage(named: "background") {
-            let backgroundImageView = UIImageView(frame: view.bounds)
-            backgroundImageView.image = backgroundImage
-            backgroundImageView.contentMode = .scaleAspectFill
-            backgroundImageView.clipsToBounds = true
-            view.insertSubview(backgroundImageView, at: 0)
-        }
-    }
-
-    private func setupNavigationBar() {
-        navigationController?.navigationBar.tintColor = UIColor.systemBackground
-        let navBarAppearance = UINavigationBarAppearance()
-        navBarAppearance.configureWithOpaqueBackground()
-
-        navBarAppearance.backgroundColor = .clear // #F8F0E5
-        navBarAppearance.shadowColor = .clear
-        navigationController?.navigationBar.standardAppearance = navBarAppearance
-        navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearance
-
-        // 현재 위치 초기화 버튼
-        if let resetLocationImage = UIImage(named: "paperplane") {
-            let resetLocationButtonSize = CGSize(width: 25, height: 25)
-            UIGraphicsBeginImageContextWithOptions(resetLocationButtonSize, false, UIScreen.main.scale)
-            resetLocationImage.draw(in: CGRect(origin: .zero, size: resetLocationButtonSize))
-
-            if let resizedResetLocationImage = UIGraphicsGetImageFromCurrentImageContext() {
-                UIGraphicsEndImageContext()
-
-                let resetLocationButton = UIBarButtonItem(image: resizedResetLocationImage.withRenderingMode(.alwaysOriginal), style: .plain, target: self, action: #selector(resetLocationButtonTapped))
-                resetLocationButton.imageInsets = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 0)
-
-                navigationItem.leftBarButtonItem = resetLocationButton
-            }
-
-            UIGraphicsEndImageContext()
-        }
-
-        // 검색 화면 이동 버튼
-        if let searchPageImage = UIImage(named: "grid4") {
-            let searchPageImageSize = CGSize(width: 25, height: 25)
-            UIGraphicsBeginImageContextWithOptions(searchPageImageSize, false, UIScreen.main.scale)
-            searchPageImage.draw(in: CGRect(origin: .zero, size: searchPageImageSize))
-
-            if let resizedSearchPageImage = UIGraphicsGetImageFromCurrentImageContext() {
-                UIGraphicsEndImageContext()
-
-                let SearchPageButton = UIBarButtonItem(image: resizedSearchPageImage.withRenderingMode(.alwaysOriginal), style: .plain, target: self, action: #selector(SearchPageButtonTapped))
-                SearchPageButton.imageInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 10)
-                navigationItem.rightBarButtonItems = [SearchPageButton, navigationItem.rightBarButtonItem].compactMap { $0 }
-            }
-            UIGraphicsEndImageContext()
-        }
     }
 
     @objc func resetLocationButtonTapped() {
@@ -144,47 +53,41 @@ class MainViewController: UIViewController, MiddleViewDelegate {
     }
 
     func didTapCurrentWeatherButton() {
-        bottomCurrentWeatherView.currentWeatherView.isHidden = false
-        bottomWeatherForecastView.weatherForecastView.isHidden = true
-        bottomMyLocationView.myLocationView.isHidden = true
-        view.bringSubviewToFront(bottomCurrentWeatherView)
+        currentWeather.currentWeatherView.isHidden = false
+        forecast.weatherForecastView.isHidden = true
+        myLocation.myLocationView.isHidden = true
+        view.bringSubviewToFront(currentWeather)
     }
 
     func didTapWeatherForecastButton() {
-        bottomCurrentWeatherView.currentWeatherView.isHidden = true
-        bottomWeatherForecastView.weatherForecastView.isHidden = false
-        bottomMyLocationView.myLocationView.isHidden = true
-        view.bringSubviewToFront(bottomWeatherForecastView)
+        currentWeather.currentWeatherView.isHidden = true
+        forecast.weatherForecastView.isHidden = false
+        myLocation.myLocationView.isHidden = true
+        view.bringSubviewToFront(forecast)
     }
 
     func didTapMyLocationButton() {
-        bottomCurrentWeatherView.currentWeatherView.isHidden = true
-        bottomWeatherForecastView.weatherForecastView.isHidden = true
-        bottomMyLocationView.myLocationView.isHidden = false
-        view.bringSubviewToFront(bottomMyLocationView)
+        currentWeather.currentWeatherView.isHidden = true
+        forecast.weatherForecastView.isHidden = true
+        myLocation.myLocationView.isHidden = false
+        view.bringSubviewToFront(myLocation)
+    }
+    
+    init() {
+        self.currentWeather = mainView.bottomCurrentWeatherView
+        self.forecast = mainView.bottomWeatherForecastView
+        self.myLocation = mainView.bottomMyLocationView
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
 
 extension MainViewController: MKMapViewDelegate {
-    // UI 관련 로직은 View에서 해야함.
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-        if let annotation = annotation as? MapAnnotation {
-            let identifier = "customAnnotation"
-            var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
-
-            if annotationView == nil {
-                annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: identifier)
-                annotationView?.image = UIImage(named: "pin")
-                annotationView?.canShowCallout = true
-                let imageSize = CGSize(width: 20, height: 25)
-                annotationView?.frame = CGRect(origin: .zero, size: imageSize)
-            } else {
-                annotationView?.annotation = annotation
-            }
-
-            return annotationView
-        }
-        return nil
+        viewModel?.mapView(mapView, viewFor: annotation)
     }
 }
 
@@ -203,22 +106,3 @@ extension MainViewController: CLLocationManagerDelegate {
         }
     }
 
-    // MARK: - Preview
-
-    struct MainViewController_Previews: PreviewProvider {
-        static var previews: some View {
-            MainVCRepresentable()
-                .edgesIgnoringSafeArea(.all)
-        }
-    }
-
-    struct MainVCRepresentable: UIViewControllerRepresentable {
-        func makeUIViewController(context: Context) -> UIViewController {
-            let mainViewController = MainViewController()
-            return UINavigationController(rootViewController: mainViewController)
-        }
-
-        func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {}
-
-        typealias UIViewControllerType = UIViewController
-    }
