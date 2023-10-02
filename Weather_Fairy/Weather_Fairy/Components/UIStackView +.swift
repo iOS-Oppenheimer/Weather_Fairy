861618d3464026317a9a2b222dc509dc3c61c654
Luna828 <eskw0701@naver.com>
2023-10-02 15:30:58 +0900

    [FEAT]: StackView alignment 센터 변경

--- Weather_Fairy/Weather_Fairy/Components/UIStackView +.swift
+++ Weather_Fairy/Weather_Fairy/Components/UIStackView +.swift
@@ -15,4 +15,18 @@ extension UIStackView {
         self.alignment = .leading
         self.spacing = spacing
     }
+    
+    func verticalStackViewForCenter(spacing: CGFloat){
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.axis = .vertical
+        self.alignment = .center
+        self.spacing = spacing
+    }
+    
+    func horizontalStackViewForCenter(spacing: CGFloat){
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.axis = .horizontal
+        self.alignment = .center
+        self.spacing = spacing
+    }
 }
